6fba743abb0056b3053b7121d54fbaa7e441952b
Konstantin Ponomarev <bronnice.kurama@gmail.com>
2022-12-09 14:29:35 +0300

    Enabled security

--- src/main/java/com/web/dictionaryservice/dictionaryservicewebimplementation/configs/WebSecurityConfig.java
+++ src/main/java/com/web/dictionaryservice/dictionaryservicewebimplementation/configs/WebSecurityConfig.java
@@ -56,7 +56,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.authorizeRequests()
-				.antMatchers("/api/auth/**").permitAll();
+				.antMatchers("/api/auth/**").permitAll()
+				.antMatchers("/api/create/**").permitAll();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
